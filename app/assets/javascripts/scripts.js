eaf093ba897a922529cf9206548cd218933751fc
Valoch <val.selve@gmail.com>
2018-12-21 00:38:12 +0100

    fix open graph

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -11,14 +11,14 @@ window.setInterval(function(){
   if (detailsShowed == true) {
     detailsExpositionElt.style.display = "none"
     vernissageAnnounceElt.style.display = "block"
-    console.log("#vernissage-anounce - display :" + vernissageAnnounceElt.style.display)
-    console.log("#details-exposition - display :" + detailsExpositionElt.style.display)
+    // console.log("#vernissage-anounce - display :" + vernissageAnnounceElt.style.display)
+    // console.log("#details-exposition - display :" + detailsExpositionElt.style.display)
     detailsShowed = false
   } else {
     detailsExpositionElt.style.display = "block"
     vernissageAnnounceElt.style.display = "none"
-    console.log("#vernissage-anounce - display :" + vernissageAnnounceElt.style.display)
-    console.log("#details-exposition - display :" + detailsExpositionElt.style.display)
+    // console.log("#vernissage-anounce - display :" + vernissageAnnounceElt.style.display)
+    // console.log("#details-exposition - display :" + detailsExpositionElt.style.display)
     detailsShowed = true
   }
 }, 3000);
